b2784fde0ede2cc1d9e4700cf96767c16d71861a
Mobye2 <121839655+Mobye2@users.noreply.github.com>
2023-08-18 17:18:13 +0800

    fine_eps

--- build_history/build_eps.py
+++ build_history/build_eps.py
@@ -3,46 +3,59 @@ import os
 import function.file_dir as file_dir
 import pandas as pd
 
-def calculate_eps(stock_id,start_date):
 
+def import_eps(stock_id, stock_name, start_date):
     api = DataLoader()
     # api.login_by_token(api_token='token')
     # api.login(user_id='user_id',password='password')
-
-    #取得EPS
+    # 取得EPS
     data = api.taiwan_stock_financial_statement(
         stock_id,
         start_date,
     )
-    data=data[data['type']=='EPS']
-    data.rename(columns={'value': 'Eps'}, inplace=True)
-    data['last_year_eps']=data['Eps'].rolling(4).sum()
-    data=data[['date', 'stock_id', 'Eps', 'last_year_eps']]
-    print('build_eps_data finished')
+    data = data[data["type"] == "EPS"]
+    data.rename(columns={"value": "Eps"}, inplace=True)
+    data["last_year_eps"] = data["Eps"].rolling(4).sum()
+    data = data[["date", "stock_id", "Eps", "last_year_eps"]]
+    print("build_eps_data finished")
 
-    #取得淨值比PBR 本益比PER 現金殖利率dividend_yield
+    # 取得淨值比PBR 本益比PER 現金殖利率dividend_yield
+    PBR = api.taiwan_stock_per_pbr(stock_id, start_date)
 
-    PBR = api.taiwan_stock_per_pbr(
-        stock_id,
-        start_date='2019-01-01'
+    #merge data and PBR with date and stock_id
+    data = pd.merge(data, PBR, on=["date", "stock_id"], how="outer")
+    # Concat with price data , fill price to the row with eps
+    price_data = pd.read_csv(
+        os.getcwd() + file_dir.price_dir + stock_id + "_" + stock_name + ".csv",
+        dtype={"stock_id": str},
+        index_col=False,
     )
-    #Concat with price data , fill price to the row with eps 
-    price_data=pd.read_csv(os.getcwd()+file_dir.price_dir+"/3231_緯創.csv", dtype={'stock_id': str}, index_col=False)
-    result_df = pd.merge(data, price_data, on=['date','stock_id'],how='outer')
-    result_df.sort_values(by='date', inplace=True)  # Sort the DataFrame by the 'date' column in ascending order
-    result_df['empty_price'] = result_df['close'].isnull()
-    # Use fillna method to fill missing 'price' data with the last row's 'price'
-    result_df['close'].fillna(method='ffill', inplace=True)
-    print (result_df)
+    result_df = pd.merge(data, price_data, on=["date", "stock_id"], how="outer")
+    result_df.sort_values(
+        by="date", inplace=True
+    )  
+    # Use fillna method to fill missing 'close'and 'PBR' data with the last row's 'price'
+    result_df["empty_price"] = result_df["close"].isnull()
+    result_df["close"].fillna(method="ffill", inplace=True)
+#    result_df.fillna(method="ffill", inplace=True)
+
     return result_df
-    # chip=pd.read_csv(os.getcwd()+file_dir.chip_dir+"/3231_緯創.csv", dtype={'stock_id': str}, index_col=False)
-    # price_chip=pd.merge(result_df,chip,on=['date','stock_id'],how='outer')
 
-if __name__ == '__main__':
-    stock_id="3231"
-    start_date='2019-01-01'
-    result_df=calculate_eps(stock_id,start_date)
 
-    filename = stock_id + '.csv'
-    result_df.to_csv(os.getcwd()+"/Data/eps_record/"+filename,
-                encoding='utf-8-sig', index=False)
+
+if __name__ == "__main__":
+    start_date = "2019-01-01"
+    tracing_list = "tracing_stock_list.csv"
+    stock_id_list = pd.read_csv(
+        os.getcwd() + "/" + tracing_list, dtype={"stock_id": str}, index_col=False
+    )
+    for index, d in stock_id_list.iterrows():
+        stock_id = d["stock_id"]
+        stock_name = d["stock_name"]
+        print(stock_id, stock_name)
+        chips_price_data = import_eps(stock_id, stock_name, start_date)
+        chips_price_data.to_csv(
+            os.getcwd() + file_dir.eps_dir + stock_id + "_" + stock_name + ".csv",
+            index=False,
+        )
+        break
